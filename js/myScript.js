eaf36e45526733c21345d234e5e211579f5df84e
Mikasaa Fx <69418658+mikasaafx@users.noreply.github.com>
2024-06-25 18:25:00 +0700

    Add files via upload

--- js/myScript.js
+++ js/myScript.js
@@ -16,3 +16,22 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+// let page = document.querySelectorAll('main');
+// let nav = document.querySelectorAll('header nav li a');
+
+// window.onscroll = () => {
+// 	page.forEach(sec => {
+// 		let top = window.scrollY;
+// 		let offset = sec.offsetTop;
+// 		let height = sec.offsetHeight;
+// 		let id = sec.getAttribute('id');
+
+// 		if (top >= offset && top < offset + height) {
+// 			navLinks.forEach(links => {
+// 				links.classList.remove('active');
+// 				document.querySelectorAll('header nav li a[href*= ' id + + ']').classList.add('active');
+// 			});
+// 		};
+// 	});
+// };
